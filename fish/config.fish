257389c279a448135f36e7bddd54efaef4279676
Yoram de Langen <yoramdelangen@gmail.nl>
2020-07-08 21:23:19 +0200

    Added push and pull command to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -18,4 +18,42 @@ alias gaa="git add --all"
 # others
 alias cc="clear"
 
-export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+function git_pull -d "Pull from current GIT repository" 
+   echo "Argv: $argv" 
+   echo "$0"
+   if test "$argv" != ""
+        echo "Pull: $argv"
+        git pull -v --stat origin "$argv"
+    else
+        set current_branch (git rev-parse --abbrev-ref HEAD)
+
+        if test "$current_branch" = ""
+            set current_branch "master"
+        end
+
+        echo "Pull: $current_branch"
+        git pull -v --stat origin "$current_branch"
+    end
+
+end
+alias pull=git_pull
+
+function git_push -d "Push from current GIT repository" 
+   if test "$argv" != ""
+        echo "Push: $argv"
+        git push -u -v --progress origin "$argv"
+    else
+        set current_branch (git rev-parse --abbrev-ref HEAD)
+
+        if test "$current_branch" = ""
+            set current_branch "master"
+        end
+
+        echo "Push: $current_branch"
+        git push -v --progress origin "$current_branch"
+    end
+
+end
+alias push=git_push
+
+export PATH="$PATH:/Applications/Visual Studio Code.app/Conte,,.,nts/Resources/app/bin"
