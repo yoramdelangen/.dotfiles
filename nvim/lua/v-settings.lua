0e09b1c888aab461981de2825601a122be529f3b
Yoram de Langen <yoramdelangen@gmail.com>
2021-12-03 08:00:34 +0100

    added: scrolloff

--- nvim/lua/v-settings.lua
+++ nvim/lua/v-settings.lua
@@ -2,8 +2,8 @@ vim.cmd('filetype plugin on')
 
 -- Custom filetypes
 vim.cmd 'autocmd BufRead,BufNewFile *.blade.php set filetype=blade'
-
 -- GLOBAL OPTIONS
+
 vim.o.hidden = true -- Required to keep multiple buffers open multiple buffers
 vim.o.title = true -- Show title??
 vim.o.mouse = "a" -- Enable your mouse
@@ -14,22 +14,36 @@ vim.o.conceallevel = 0 -- So that I can see `` in markdown files
 vim.o.expandtab = true -- use spaces instead of tabs
 vim.o.numberwidth = 5 -- Set gutter width
 vim.o.ignorecase = true -- set search cases to in-sensitive
+
 -- spacing
 vim.o.smarttab = true -- Smart tabbing?
 vim.o.autoindent = true
 vim.o.breakindent = true -- ident -> same amount of space as the beginning of that line
 
--- VIM script options
+-- script options
 vim.cmd('set ts=4') -- Insert 2 spaces for a tab
 vim.cmd('set sw=4') -- Change the number of space characters inserted for indentation
 
 -- GLOBAL WINDOW OPTIONS
 vim.wo.cursorline = true
 vim.wo.number = true
+vim.wo.scrolloff = 10
 
 -- Speed up completion windows
 vim.o.updatetime = 300 -- Faster completion
 vim.o.timeoutlen = 300 -- By default timeoutlen is 1000 ms
+
+-- disable certain default plugins
+vim.g.loaded_gzip         = 1
+vim.g.loaded_tar          = 1
+vim.g.loaded_tarPlugin    = 1
+vim.g.loaded_zipPlugin    = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_netrw        = 1
+vim.g.loaded_netrwPlugin  = 1
+vim.g.loaded_matchit      = 1
+vim.g.loaded_matchparen   = 1
+vim.g.loaded_spec         = 1
 
 -- GLOBAL BUFFER OPTIONS
 -- vim.bo.smartindent = true
