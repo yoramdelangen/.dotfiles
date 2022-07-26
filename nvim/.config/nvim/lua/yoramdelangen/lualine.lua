099b534810feffa65b271da24e84dce69e0151a6
Yoram de Langen <yoramdelangen@gmail.com>
2022-07-26 22:21:16 +0200

    improved lualine style

--- nvim/.config/nvim/lua/yoramdelangen/lualine.lua
+++ nvim/.config/nvim/lua/yoramdelangen/lualine.lua
@@ -1,39 +1,36 @@
 require('lualine').setup {
-  -- options = {
-  --   icons_enabled = true,
-  --   theme = 'auto',
+  options = {
+    icons_enabled = false,
+    theme = 'auto',
     -- component_separators = { left = '', right = ''},
-    component_separators = {},
+    component_separators = '',
     -- section_separators = { left = '', right = ''},
-    section_separators = { },
+    section_separators = '',
   --   disabled_filetypes = {
   --     statusline = {},
   --     winbar = {},
   --   },
-  --   always_divide_middle = true,
   --   globalstatus = false,
-  --   refresh = {
-  --     statusline = 1000,
-  --     tabline = 1000,
-  --     winbar = 1000,
-  --   }
-  -- },
-  sections = {
-    -- lualine_a = {'mode'},
-    lualine_b = {'branch', 'diagnostics'},
-    lualine_c = {'location', 'filename'},
-    -- lualine_x = {'encoding', 'fileformat', 'filetype'},
-    -- lualine_y = {'progress'},
-    -- lualine_z = {'location'}
+    refresh = {
+      statusline = 500,
+    }
   },
-  -- inactive_sections = {
-  --   lualine_a = {},
-  --   lualine_b = {},
-  --   lualine_c = {'filename'},
-  --   lualine_x = {'location'},
-  --   lualine_y = {},
-  --   lualine_z = {}
-  -- },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diagnostics'},
+    lualine_c = {'diff', '%=', {'filename', path=3 }},
+    lualine_x = {'fileformat', 'filetype'},
+    lualine_y = {},
+    lualine_z = {},
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'%=', {'filename', path=3 }},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
   -- tabline = {},
   -- winbar = {},
   -- inactive_winbar = {},
