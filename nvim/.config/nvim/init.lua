fe159a15cdaa1fe28f82228255ff36471f508bf1
Yoram de Langen <yoramdelangen@gmail.com>
2023-12-21 09:37:37 +0100

    update nvim config
    Signed-off-by: Yoram de Langen <yoramdelangen@gmail.com>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -37,6 +37,13 @@ I hope you enjoy your Neovim journey,
 
 P.S. You can delete this when you're done too. It's your config now :)
 --]]
+
+if vim.g.neovide then
+  vim.g.neovide_cursor_trail_legnth = 0
+  vim.g.neovide_cursor_animation_length = 0
+  vim.o.guifont = "Jetbrains Mono:h16"
+end
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
